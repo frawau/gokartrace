f4a61fe07350baf2859f9888ee4c1232510f8758
Francois Wautier <francois@wautier.eu>
2025-03-05 22:45:30 +0700

    Missing Import.

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
