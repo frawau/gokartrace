07909ab1eeb1144db7fef2ec4d86c71f6fa073f5
Francois Wautier <francois@wautier.eu>
2025-03-09 17:44:25 +0700

    ASGI issue?

--- core/asgi.py
+++ core/asgi.py
@@ -11,6 +11,8 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from home import routing
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": URLRouter(routing.websocket_urlpatterns),
