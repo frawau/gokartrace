2cd2e320899f32e4498692a6f671d861c01e89ce
Francois Wautier <francois@wautier.eu>
2025-03-10 22:06:37 +0700

    Let's try again.

--- core/settings.py
+++ core/settings.py
@@ -213,7 +213,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("192.168.77.8", 6379)],
+            "hosts": [("192.168.77.8", 8000)],
         },
     },
 }
