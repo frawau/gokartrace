7ed2e699418f69f6743467de6e2e1a8ed8635dc4
Francois Wautier <francois@wautier.eu>
2025-03-10 22:14:21 +0700

    Let's try again.

--- race/signals.py
+++ race/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -6,13 +6,19 @@ from .models import ChangeLane
 from django.template.loader import render_to_string
 
 @receiver(post_save, sender=ChangeLane)
-def change_lane_updated(sender, instance, **kwargs):
-    channel_layer = get_channel_layer()
-    lane_html = render_to_string('layout/changelane_info.html', {'change_lane': instance})
-    async_to_sync(channel_layer.group_send)(
-        f'lane_{instance.lane}', #Use instance.lane
-        {
-            'type': 'lane.update',
-            'lane_html': lane_html,
-        }
-    )
+def change_lane_updated(sender, instance, created, **kwargs):
+    if not created:  # Only send updates if the instance was modified
+        channel_layer = get_channel_layer()
+        lane_html = render_to_string('race/changelane_info.html', {'change_lane': instance})
+        async_to_sync(channel_layer.group_send)(
+            f'lane_{instance.lane}',
+            {
+                'type': 'lane.update',
+                'lane_html': lane_html,
+            }
+        )
+
+@receiver(post_delete, sender=ChangeLane)
+def change_lane_deleted(sender, instance, **kwargs):
+    #Add logic here if you want to send a websocket message when a lane is deleted.
+    pass
