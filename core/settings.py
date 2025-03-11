aa25aac2c8ecd9389cfd3badd3e11da1014cc4e0
Francois Wautier <francois@wautier.eu>
2025-03-11 19:28:40 +0700

    Grrr.

--- core/settings.py
+++ core/settings.py
@@ -213,7 +213,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("192.168.77.8", 8000)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
