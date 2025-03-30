d675f5fbf2d341e22d9da63290a2c88435651b79
Francois Wautier <francois@wautier.eu>
2025-03-30 14:17:30 +0700

    Correct import.

--- race/management/commands/initialisedb.py
+++ race/management/commands/initialisedb.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.contrib.auth.models import Group
 from django.core.validators import MinValueValidator, MaxValueValidator
-from .models import (
+from race.models import (
     Person,
     Team,
     Championship,
