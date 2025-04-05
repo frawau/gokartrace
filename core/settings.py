243eaa7dd30b7c96ca948dc5a21c1000e567b781
Francois Wautier <francois@wautier.eu>
2025-04-05 10:35:26 +0700

    Damned.

--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    # "debug_toolbar",
     "channels",
     "daphne",
     "race",
