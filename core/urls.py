2f132a5836abcd6b03fb78b747eedad369307467
Francois Wautier <francois@wautier.eu>
2025-04-12 13:30:42 +0700

    Tailored login form.

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ except:
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("race.urls")),
-    path("", include("theme_material_kit.urls")),
+    # path("", include("theme_material_kit.urls")),
     path("login/jwt/", view=obtain_auth_token),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + debug_toolbar_urls()
