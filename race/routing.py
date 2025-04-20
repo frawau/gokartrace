b9fa3dff3458a2df97c1450b695077feb9eb6b7e
Francois Wautier <francois@wautier.eu>
2025-04-20 14:50:33 +0700

    New timer widget.

--- race/routing.py
+++ race/routing.py
@@ -6,6 +6,6 @@ websocket_urlpatterns = [
     path("ws/changedriver/", consumers.ChangeDriverConsumer.as_asgi()),
     path("ws/roundpause/<int:round_id>/", consumers.RoundPauseConsumer.as_asgi()),
     path("ws/empty_teams/", consumers.EmptyTeamsConsumer.as_asgi()),
-    path('ws/round/(?P<round_id>\d+)/', consumers.RoundConsumer.as_asgi()),
-    path('ws/roundpause/(?P<round_id>\d+)/', consumers.RoundPauseConsumer.as_asgi()),
+    path('ws/round/<int:round_id>/', consumers.RoundConsumer.as_asgi()),
+    path('ws/roundpause/<int:round_id>/', consumers.RoundPauseConsumer.as_asgi()),
 ]
