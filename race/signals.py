df44d9c9d1cb1f93f62a1d392d89c6c34c2266aa
Francois Wautier <francois@wautier.eu>
2025-04-26 16:23:32 +0700

    Correct at start. Handle deleted sessions.

--- race/signals.py
+++ race/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
