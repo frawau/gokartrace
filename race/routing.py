6e5b604294bf09252c92be4ffd0697f413b9a264
Francois Wautier <francois@wautier.eu>
2025-04-26 11:00:06 +0700

    Improve signal/consumer... forgot one.

--- race/routing.py
+++ race/routing.py
@@ -4,7 +4,6 @@ from . import consumers
 websocket_urlpatterns = [
     path("ws/pitlanes/<int:pitlane_number>/", consumers.ChangeLaneConsumer.as_asgi()),
     path("ws/changedriver/", consumers.ChangeDriverConsumer.as_asgi()),
-    path("ws/roundpause/<int:round_id>/", consumers.RoundPauseConsumer.as_asgi()),
     path("ws/empty_teams/", consumers.EmptyTeamsConsumer.as_asgi()),
     path("ws/round/<int:round_id>/", consumers.RoundConsumer.as_asgi()),
 ]
