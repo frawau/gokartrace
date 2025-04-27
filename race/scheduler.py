5de6a31b95bff922c4c442f397b9680fb6adefee
Francois Wautier <francois@wautier.eu>
2025-04-27 12:45:51 +0700

    Added tasks to open and close pit lanes. Added context to get pitlanes menu entries in navigation.

--- race/scheduler.py
+++ race/scheduler.py
@@ -1,9 +1,12 @@
 # race/scheduler.py
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from django_apscheduler.jobstores import DjangoJobStore
-
+import asyncio as aio
 
 def racing_start():
+
+    loop = aio.new_event_loop()
+    aio.set_event_loop(loop)
     scheduler = AsyncIOScheduler()
     scheduler.add_jobstore(DjangoJobStore(), "default")
 
@@ -11,5 +14,13 @@ def racing_start():
     from race.tasks import RaceTasks  # import here to avoid circular imports
 
     scheduler.add_job(RaceTasks.race_events, "interval", minutes=1, id="race-event-task")
+    # Start in a background thread
+    import threading
+    thread = threading.Thread(target=start_loop, args=(loop, scheduler))
+    thread.daemon = True
+    thread.start()
 
+def start_loop(loop, scheduler):
+    asyncio.set_event_loop(loop)
     scheduler.start()
+    loop.run_forever()
